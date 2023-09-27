edc576d35a069db5d740716f0221de66f629c466
Maulana Seto <maulana.seto@ui.ac.id>
2023-09-27 11:05:06 +0700

    Tugas 4

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,20 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, ValidationError
 from main.models import Product
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ["type", "name", "amount", "owner", "description"]
+        fields = ["type", "name", "owner", "amount", "description"]
+        labels = {
+            "type": "Jenis",
+            "name": "Nama",
+            "owner": "Pemilik",
+            "amount": "Jumlah",
+            "description": "Deskripsi",
+        }
+    
+    def clean_amount(self):
+        amount = self.cleaned_data['amount']
+        if amount < 0:
+            raise ValidationError('Jumlah tidak boleh negatif!')
+        return amount
--- main/models.py
+++ main/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
-    amount = models.IntegerField()
     owner = models.CharField(max_length=255)
+    amount = models.IntegerField(validators=[MinValueValidator(0)])
     date_added = models.DateField(auto_now_add=True)
     description = models.TextField()
